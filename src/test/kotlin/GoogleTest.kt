9526a21940848ec85c95cd3c4991b97de59490ac
Vladimir Kazantsev <kazantsevv.a@yandex.ru>
2022-05-24 20:04:57 +0300

    init

--- src/test/kotlin/GoogleTest.kt
+++ src/test/kotlin/GoogleTest.kt
@@ -1,10 +1,12 @@
 import com.codeborne.selenide.Selenide
+import com.codeborne.selenide.Configuration
 import org.junit.jupiter.api.Test
 
 class GoogleTest {
 
     @Test
     fun searchTest(){
+        Configuration.browserSize = "1920x1080";
         Selenide.open("https://www.google.com/")
     }
 }
